cebf5be60dfe03c2e54cb67ead32992d1caf2792
Sreehari-crayotech <sreehari@crayotech.com>
2024-03-13 18:03:29 +0530

    Exception Handling

--- lib/src/ui/profile/components/cms/how_to_redeem_my_points_screen.dart
+++ lib/src/ui/profile/components/cms/how_to_redeem_my_points_screen.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lets_collect/src/bloc/cms_bloc/how_to_redeem_my_points/how_to_redeem_my_points_bloc.dart';
+import 'package:lets_collect/src/constants/assets.dart';
 import 'package:lets_collect/src/constants/colors.dart';
+import 'package:lets_collect/src/utils/network_connectivity/bloc/network_bloc.dart';
+import 'package:lottie/lottie.dart';
 
 class HowToRedeemMyPointsScreen extends StatefulWidget {
   const HowToRedeemMyPointsScreen({super.key});
 
   @override
-  State<HowToRedeemMyPointsScreen> createState() => _HowToRedeemMyPointsScreenState();
+  State<HowToRedeemMyPointsScreen> createState() =>
+      _HowToRedeemMyPointsScreenState();
 }
 
 class _HowToRedeemMyPointsScreenState extends State<HowToRedeemMyPointsScreen> {
 
+  bool networkSuccess= false;
+
   @override
   void initState() {
     super.initState();
-    BlocProvider.of<HowToRedeemMyPointsBloc>(context).add(GetHowToRedeemMyPointsEvent());
-
+    BlocProvider.of<HowToRedeemMyPointsBloc>(context).add(
+        GetHowToRedeemMyPointsEvent());
   }
 
   @override
@@ -44,24 +51,53 @@ class _HowToRedeemMyPointsScreenState extends State<HowToRedeemMyPointsScreen> {
           ),
         ),
       ),
-      body: BlocBuilder<HowToRedeemMyPointsBloc, HowToRedeemMyPointsState>(
+      body: BlocConsumer<NetworkBloc, NetworkState>(
+        listener: (context, state) {
+          if (state is NetworkSuccess) {
+            networkSuccess = true;
+          }
+        },
         builder: (context, state) {
-          if(state is HowToRedeemMyPointsLoading) {
-            return const Center(
-              child: RefreshProgressIndicator(
-                backgroundColor: AppColors.secondaryColor,
-                color: AppColors.primaryWhiteColor,
+          if(state is NetworkSuccess){
+            return BlocBuilder<HowToRedeemMyPointsBloc, HowToRedeemMyPointsState>(
+              builder: (context, state) {
+                if (state is HowToRedeemMyPointsLoading) {
+                  return const Center(
+                    child: RefreshProgressIndicator(
+                      backgroundColor: AppColors.secondaryColor,
+                      color: AppColors.primaryWhiteColor,
+                    ),
+                  );
+                }
+                if (state is HowToRedeemMyPointsLoaded) {
+                  return SingleChildScrollView(
+                    child: Html(
+                        data: state.howToRedeemMyPointsResponse.data.pageContent),
+                  );
+                }
+                return const Center(
+                  child: Text("No Data to show"),
+                );
+              },
+            );
+          }else if (state is NetworkFailure) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Lottie.asset(Assets.NO_INTERNET),
+                  Text(
+                    "You are not connected to the internet",
+                    style: GoogleFonts.openSans(
+                      color: AppColors.primaryGrayColor,
+                      fontSize: 20,
+                    ),
+                  ).animate().scale(delay: 200.ms, duration: 300.ms),
+                ],
               ),
             );
           }
-          if(state is HowToRedeemMyPointsLoaded) {
-            return SingleChildScrollView(
-              child: Html(data: state.howToRedeemMyPointsResponse.data.pageContent),
-            );
-          }
-          return const Center(
-            child: Text("No Data to show"),
-          );
+          return const SizedBox();
         },
       ),
     );
--- lib/src/ui/profile/components/cms/point_calculations_screen.dart
+++ lib/src/ui/profile/components/cms/point_calculations_screen.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lets_collect/src/bloc/cms_bloc/point_calculations/point_calculations_bloc.dart';
+import 'package:lets_collect/src/constants/assets.dart';
 import 'package:lets_collect/src/constants/colors.dart';
+import 'package:lets_collect/src/utils/network_connectivity/bloc/network_bloc.dart';
+import 'package:lottie/lottie.dart';
 
 class PointCalculationsScreen extends StatefulWidget {
   const PointCalculationsScreen({super.key});
 
+
   @override
-  State<PointCalculationsScreen> createState() => _PointCalculationsScreenState();
+  State<PointCalculationsScreen> createState() =>
+      _PointCalculationsScreenState();
 }
 
 class _PointCalculationsScreenState extends State<PointCalculationsScreen> {
 
+  bool networkSuccess = false;
+
+
   @override
   void initState() {
     super.initState();
-    BlocProvider.of<PointCalculationsBloc>(context).add(GetPointCalculationsEvent());
+    BlocProvider.of<PointCalculationsBloc>(context).add(
+        GetPointCalculationsEvent());
   }
 
   @override
@@ -43,24 +53,53 @@ class _PointCalculationsScreenState extends State<PointCalculationsScreen> {
           ),
         ),
       ),
-      body: BlocBuilder<PointCalculationsBloc, PointCalculationsState>(
+      body: BlocConsumer<NetworkBloc, NetworkState>(
+        listener: (context, state) {
+          if (state is NetworkSuccess) {
+            networkSuccess = true;
+          }
+        },
         builder: (context, state) {
-          if(state is PointCalculationsLoading) {
-            return const Center(
-              child: RefreshProgressIndicator(
-                backgroundColor: AppColors.secondaryColor,
-                color: AppColors.primaryWhiteColor,
+          if(state is NetworkSuccess){
+            return BlocBuilder<PointCalculationsBloc, PointCalculationsState>(
+              builder: (context, state) {
+                if (state is PointCalculationsLoading) {
+                  return const Center(
+                    child: RefreshProgressIndicator(
+                      backgroundColor: AppColors.secondaryColor,
+                      color: AppColors.primaryWhiteColor,
+                    ),
+                  );
+                }
+                if (state is PointCalculationsLoaded) {
+                  return SingleChildScrollView(
+                    child: Html(
+                        data: state.pointCalculationsResponse.data.pageContent),
+                  );
+                }
+                return const Center(
+                  child: Text("No Data to show"),
+                );
+              },
+            );
+          }else if (state is NetworkFailure) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Lottie.asset(Assets.NO_INTERNET),
+                  Text(
+                    "You are not connected to the internet",
+                    style: GoogleFonts.openSans(
+                      color: AppColors.primaryGrayColor,
+                      fontSize: 20,
+                    ),
+                  ).animate().scale(delay: 200.ms, duration: 300.ms),
+                ],
               ),
             );
           }
-          if(state is PointCalculationsLoaded) {
-            return SingleChildScrollView(
-              child: Html(data: state.pointCalculationsResponse.data.pageContent),
-            );
-          }
-          return const Center(
-            child: Text("No Data to show"),
-          );
+          return const SizedBox();
         },
       ),
     );
